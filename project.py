34486eaa6452ef9793a6a1ee1297d162efbe6bf8
Saachi Raju <saachi.raju@gmail.com>
2024-11-22 19:54:58 -0800

    Forward Selection Algorithm Commit

--- project.py
+++ project.py
@@ -4,6 +4,63 @@ def evaluate_feature_set(selected_features):
     # function that will generate random
     return random.uniform(0, 100)
 
+def forward_selection(total_features):
+    # Initialize the current feature set as empty
+    current_features = set()
+    best_overall_accuracy = 0
+    best_feature_set = set()
+
+    # Evaluate accuracy with no features
+    initial_accuracy = evaluate_feature_set(current_features)
+    # text required in project guideline
+    print('Using no features and "random" evaluation, I get an accuracy of {:.1f}%'.format(initial_accuracy))
+    print("Beginning search.")
+
+    for i in range(1, total_features + 1):
+        feature_to_add_at_this_level = None
+        best_accuracy_so_far = 0
+
+        for feature in range(1, total_features + 1):
+            if feature not in current_features:
+                # Try adding this feature to the current set
+                features_to_evaluate = current_features.copy()
+                features_to_evaluate.add(feature)
+
+                # Evaluate the accuracy of this set
+                accuracy = evaluate_feature_set(features_to_evaluate)
+                print("Using feature(s) {} accuracy is {:.1f}%".format(
+                    sorted(features_to_evaluate), accuracy)) # gets the best accruacy
+
+                # update if this subset gives the best accuracy so far
+
+                
+                if accuracy >= best_accuracy_so_far:
+                    best_accuracy_so_far = accuracy
+                    feature_to_add_at_this_level = feature
+                    best_features_so_far = features_to_evaluate.copy()
+
+        # If a feature was identified to add
+        if feature_to_add_at_this_level is not None:
+            current_features.add(feature_to_add_at_this_level)
+
+            # Update the best overall subset if this accuracy is higher
+            if best_accuracy_so_far >= best_overall_accuracy:
+                best_overall_accuracy = best_accuracy_so_far
+                best_feature_set = current_features.copy()
+
+                # Print the best feature set and accuracy for this iteration
+                print("Feature set {} was best, accuracy is {:.1f}%".format(
+                    sorted(current_features), best_accuracy_so_far))
+            else:
+                # No improvement in accuracy
+                print("(Warning, Accuracy has decreased!)") # project guideline
+                break 
+        else:
+            break
+
+    # print statements from the guideline
+    print("Finished search!! The best feature subset is {}, which has an accuracy of {:.1f}%".format(
+        sorted(best_feature_set), best_overall_accuracy))
 
 def backward_elimination(total_features):
     # Initialize the current feature set with all features
@@ -61,16 +118,16 @@ def backward_elimination(total_features):
 
 
 def main():
-    print("Welcome to Your Name's Feature Selection Algorithm.")
+    print("Welcome to Akanksh and Saachi's Feature Selection Algorithm.")
     total_features = int(input("Please enter total number of features: "))
     print("Type the number of the algorithm you want to run.")
     print("1) Forward Selection")
     print("2) Backward Elimination")
     choice = input()
 
-    # if choice == '1':
-    #     # test
-    if choice == '2':
+    if choice == '1':
+        forward_selection(total_features)
+    elif choice == '2':
         backward_elimination(total_features)
     else:
         print("Invalid choice or special algorithm not implemented.")
